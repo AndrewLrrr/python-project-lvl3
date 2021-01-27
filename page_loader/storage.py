ae2af3a70ec77e626ff016417b85cb3e05acebe7
Andrey Larin <larin.projects@gmail.com>
2021-01-27 15:00:49 +0300

    Change makedirs -> mkdir

--- page_loader/storage.py
+++ page_loader/storage.py
@@ -8,7 +8,7 @@ def save(file_path: str, data: Union[str, bytes]) -> int:
     dir_path = os.path.dirname(file_path)
 
     if not os.path.exists(dir_path):
-        os.makedirs(dir_path, DIRECTORY_ACCESS_RIGHTS)
+        os.mkdir(dir_path, DIRECTORY_ACCESS_RIGHTS)
 
     mode, encoding = ('w', 'utf8') if isinstance(data, str) else ('wb', None)
     with open(file_path, mode=mode, encoding=encoding) as f:
